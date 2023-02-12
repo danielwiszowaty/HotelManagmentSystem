f8610a348c7f8273cdfb2a38d8367b9a34901b33
Daniel Wiszowaty <daniel.wiszowaty99@gmail.com>
2023-02-12 18:20:52 +0100

    Init project

---
+++ src/main/java/pl/danielwiszowaty/hotelmanagmentsystem/HotelManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.danielwiszowaty.hotelmanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagmentSystemApplication.class, args);
+    }
+
+}
