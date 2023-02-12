33119234395d4573fc2b73b6995da74ff15ca687
Daniel Wiszowaty <daniel.wiszowaty99@gmail.com>
2023-02-12 18:23:46 +0100

    Javadoc for main function

--- src/main/java/pl/danielwiszowaty/hotelmanagmentsystem/HotelManagmentSystemApplication.java
+++ src/main/java/pl/danielwiszowaty/hotelmanagmentsystem/HotelManagmentSystemApplication.java
@@ -3,6 +3,9 @@ package pl.danielwiszowaty.hotelmanagmentsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Daniel
+ */
 @SpringBootApplication
 public class HotelManagmentSystemApplication {
 
